5fdfbe9a8d20b0a717491344d519e26736325b22
SilvaGustavoo <g66587404@gmail.com>
2022-08-30 08:22:39 -0300

    Commit não feito anteriormente (outro computador)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function html() {
+    document.getElementById('habilidades').style="background-color: #000000; transition: .3s; background: #522301";
+
+    document.getElementById('sombra').style="left: 340px; background-color: orange; filter: opacity(1.0) blur(150px);";
+}
+
 function sql() {
-    document.getElementById('habilidades').style="background: #AAA";
+    document.getElementById('sombra').style="filter: opacity(1.0) blur(60px); "
+
+    document.getElementById('habilidades').style="background: #000000;transition: 1s;"
+
+    
 }
