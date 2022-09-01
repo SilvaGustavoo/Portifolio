5b639da8730d932e5a25ace0e313c9ab4aaa75d7
SilvaGustavoo <g66587404@gmail.com>
2022-09-01 16:34:44 -0300

    Responsividades - 900 pixels e 420 pixels ou menos (finalizados)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function sql(){
-    document.getElementById("sql_sombra").style="filter: opacity(1) blur(50px)";
+    document.getElementById("sql_sombra").style="filter: opacity(1) blur(60px)";
 
     document.getElementById("article_sombra").style="filter: opacity(1) blur(70px); background: white";
 
@@ -9,7 +9,11 @@ function sql(){
 
     document.getElementById("description_tec").innerText="Modelos Conceituais e Realacionais tenho um breve conhecimento. Fiz uso do Mysql Workbench, nada profissional...<br>Se quiser acessar meus projetos e criações clique na imagem e veja meu repositório !!";
 
-    document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;";
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        document.getElementById("description_tec").style="margin-top: 50px;"
+    } else {
+        document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;";
+    }
 
     document.getElementById("article").style="color: black";
 
@@ -40,7 +44,11 @@ function html() {
 
     document.getElementById("description_tec").innerText="Tenho conhecimentos da base do html, como sua estrutura semantica recomendada e tambem sobre o uso de suas tags para elaboração de tabelas e formularios. "
 
-    document.getElementById("description_tec").style="    margin-top: 30px;line-height: normal;"
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        document.getElementById("description_tec").style="margin-top: 50px;"
+    } else {
+        document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;";
+    }
 
     document.getElementById("article").style="color: white;"
 
@@ -71,7 +79,11 @@ function scrum() {
 
     document.getElementById("description_tec").innerText="como realizar muito mais tarefas em menos tempo? Scrum me ensinou isso, aderir essa metodologia na vida profissional e pessoal é uma escolha da qual não tem volta!! Tenho conhecimentos tambem em Kanban e outras"
 
-    document.getElementById("description_tec").style="    margin-top: 30px;line-height: normal;"
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        document.getElementById("description_tec").style="margin-top: 50px;"
+    } else {
+        document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;";
+    }
 
     document.getElementById("article").style=""
 
@@ -102,7 +114,11 @@ function css() {
 
     document.getElementById("description_tec").innerText="Conhecimentos em estilização se estende em desenvolvimento de paginas responsivas e divertidas com auxilio aplicativos, como Figma ou Marvel."
 
-    document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;"
+    if (window.matchMedia("(max-width: 480px)").matches) {
+        document.getElementById("description_tec").style="margin-top: 50px;"
+    } else {
+        document.getElementById("description_tec").style="margin-top: 30px;line-height: normal;";
+    }
 
     document.getElementById("article").style="color: white;";
 
